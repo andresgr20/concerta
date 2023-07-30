655d5e9a0a1d89b5aff58463132b60e4237e5b7c
Qiye Chen <qiyechen@amazon.com>
2023-07-29 22:51:20 -0400

    Setting up Spring

---
+++ src/main/java/com/pinkeagle/concerta/ConcertaApplication.java
@@ -0,0 +1,13 @@
+package com.pinkeagle.concerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinkeagle/concerta/ConcertaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinkeagle.concerta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
